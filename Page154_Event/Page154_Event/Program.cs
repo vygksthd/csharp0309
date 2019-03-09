81f46450cce0bfb683c86a179a8c8fca03b91de5
junhong cheon <cjswnsghd12@gmail.com>
2019-03-09 16:51:41 +0900

    Page154_Event

---
+++ Page154_Event/Page154_Event/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Page154_Event
+{
+    class EventPublisher
+    {
+        public delegate void MyEventHandler();
+        public event MyEventHandler MyEvent;
+        public void Do()
+        {
+            MyEvent?.Invoke();
+        }
+    }
+    class Subscriber
+    {
+        static void Main(string[] args)
+        {
+            EventPublisher p = new EventPublisher();
+            p.MyEvent += new EventPublisher.MyEventHandler(doAction);
+            p.Do();
+        }
+        static void doAction()
+        {
+            Console.WriteLine("MyEvent라는 이벤트 발생...");
+        }
+    }
+}
