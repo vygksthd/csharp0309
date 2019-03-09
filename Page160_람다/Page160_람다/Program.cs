f81c2606a4e38ff9d40bbe273a1fc29cae591fa1
junhong cheon <cjswnsghd12@gmail.com>
2019-03-09 17:54:17 +0900

    Page160_람다

---
+++ Page160_람다/Page160_람다/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page160_람다
+{
+    class Program
+    {
+        delegate int Calc(int i, int j);
+
+        static void Main(string[] args)
+        {
+            Calc c = new Calc(MySum);
+            Console.WriteLine("1 + 2 = {0}", c(1, 2));
+
+            Calc c1 = delegate (int i, int j)
+            {
+                return i + j;
+            };
+            Console.WriteLine("3 + 4 = {0}", c1(3, 4));
+
+            Calc c2 = (int a, int b) => a + b;
+            Console.WriteLine("3 + 4 = {0}", c2(3, 4));
+
+            Calc c3 = (a, b) => a + b;
+            Console.WriteLine("3 + 4 = {0}", c3(3, 4));
+        }
+
+        static int MySum(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
