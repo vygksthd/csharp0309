1b1fad184ab90c9712199d8f957668cd66a28538
junhong cheon <cjswnsghd12@gmail.com>
2019-03-09 13:51:17 +0900

    Delegate_Func<>

--- Page140_Delegate/Page140_Delegate/Delegate.cs
+++ Page140_Delegate/Page140_Delegate/Delegate.cs
@@ -7,11 +7,10 @@ using System.Threading.Tasks;
 namespace Page140_Delegate
 {    
     class Delegate
-    {
-        private delegate int OnjSum(int i, int j);
+    { 
         static void Main(string[] args)
         {
-            OnjSum myMethod = new OnjSum(Delegate.Sum);
+            Func<int, int, int> myMethod = Sum;
             Console.WriteLine("두수 합 : {0}", myMethod(10, 30));
         }
         static int Sum(int i, int j)
