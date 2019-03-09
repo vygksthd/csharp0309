628f3548ee3312ff5d84147a5cbcf38244600070
junhong cheon <cjswnsghd12@gmail.com>
2019-03-09 17:06:24 +0900

    Page155_EventHandler

---
+++ Page155_EventHandler/Page155_EventHandler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Page155_EventHandler
+{
+    class EventPublisher
+    {
+        public event EventHandler MyEvent;
+        public void Do()
+        {
+            if(MyEvent != null)
+            {
+                MyEvent(null, null);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventPublisher p = new EventPublisher();
+            p.MyEvent += new EventHandler(doAction);
+            p.Do();
+        }
+
+        static void doAction(object sender, EventArgs e)
+        {
+            Console.WriteLine("MyEvent라는 이벤트 발생...");
+        }
+    }
+}
