6a19cbad4606b4076bd04e9e398c58c1bcfe51c8
junhong cheon <cjswnsghd12@gmail.com>
2019-03-09 18:13:56 +0900

    Page163_문형식_람다

---
+++ Page163_문형식 람다/Page163_문형식 람다/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page163_문형식_람다
+{
+    class Program
+    {
+        delegate int Sum(int[] arg);
+        static void Main(string[] args)
+        {
+            Sum sumdeli = (arg) =>
+            {
+                int mySum = 0;
+                foreach (int i in arg)
+                {
+                    mySum += i;
+                }
+                return mySum;
+            };
+            int sum = sumdeli(new int[] { 1, 2, 3, 4, 5, });
+            Console.WriteLine("1+2+3+4+5=" + sum);
+        }
+    }
+}
