7ac0c5f15411677142618f4acd52f7cfdbbc67ad
junhong cheon <cjswnsghd12@gmail.com>
2019-03-09 10:49:41 +0900

    Page133_일반화클래스

---
+++ Page133_일반화클래스/Page133_일반화클래스/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page133_일반화클래스
+{
+    class Ojc<T>
+    {
+        private T[] ojcArr = new T[10];
+
+        public T this[int i]
+        {
+            get { return ojcArr[i]; }
+            set { ojcArr[i] = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ojc<string> ojc1 = new Ojc<string>();
+            ojc1[0] = "Hello, OJC";
+            Console.WriteLine(ojc1[0]);
+
+            Ojc<int> ojc2 = new Ojc<int>();
+            ojc2[0] = 999;
+            Console.WriteLine(ojc2[0]);
+        }
+    }
+}
